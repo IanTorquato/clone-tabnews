48910f571ef7c24bda705f554c1eb40ac0601422
Ian Torquato <iantorquato2@gmail.com>
2023-11-15 20:26:50 -0300

    Add the `prettier` dependency

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <h1>Me manda uma mensagem de sua escolha com PIX pra iantorquato2@gmail.com</h1>
+      <h1>
+        Me manda uma mensagem de sua escolha com PIX pra iantorquato2@gmail.com
+      </h1>
 
       <h2>A Ana é muita cara bixo 🥺🥲</h2>
     </div>
-  )
+  );
 }
