2ec797289e8e3cb9f059255f848fa7fc7a0c7b18
Ian Torquato <iantorquato2@gmail.com>
2023-11-15 14:57:21 -0300

    Edit Home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Me manda uma mensagem de sua escolha com PIX pra iantorquato2@gmail.com</h1>
+  return (
+    <div>
+      <h1>Me manda uma mensagem de sua escolha com PIX pra iantorquato2@gmail.com</h1>
+
+      <h2>A Ana é muita cara bixo 🥺🥲</h2>
+    </div>
+  )
 }
